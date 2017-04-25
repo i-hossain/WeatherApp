3e3af5d1d59b9ae75108e7dd60c163788c914b2e
connected-ihossain <ihossain@connectedlab.com>
2017-04-25 06:16:37 -0400

    Set UI elements on UIView

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ismail Hossain on 2017-04-24.
+//  Copyright © 2017 Ismail Hossain. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
